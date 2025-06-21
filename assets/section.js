f1a6f6c74ebcd17f545cc03ac1f03c4f04187d86
Mayank Kumar <mayank12999@gmail.com>
2025-06-21 13:31:22 +0530

    Delete assets/section.js

--- assets/section.js
+++
@@ -1,7 +0,0 @@
-
-const toggleBtn = document.querySelector('.toggle-btn');
-if (toggleBtn) {
-  toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('light-mode');
-  });
-}
