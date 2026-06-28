a825389f558c4f58b3669735e35e2f36a6ff069a
Mayank Kumar <mayank12999@gmail.com>
2026-06-28 20:29:13 +0530

    Deploy MoonOS 98 portfolio

--- src/components/shared/WindowButton.tsx
+++
@@ -1,5 +0,0 @@
-import { ButtonHTMLAttributes } from "react";
-
-export function WindowButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <button {...props} className={`win-button ${props.className ?? ""}`} />;
-}
